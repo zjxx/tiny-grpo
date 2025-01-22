9ebe4fe8be3d0e142bfeeffe519dfa912ae754ed
Andreas Koepf <andreas.koepf@aleph-alpha.com>
2025-01-22 21:56:54 +0100

    export of tiny-grpo in own repo

---
+++ utils.py
@@ -0,0 +1,10 @@
+import json
+from pathlib import Path
+from typing import Iterator
+
+
+def read_jsonl(file_name: str | Path) -> Iterator:
+    file_path = Path(file_name)
+    with file_path.open(mode="r", encoding="utf-8") as f:
+        for line in f:
+            yield json.loads(line)
